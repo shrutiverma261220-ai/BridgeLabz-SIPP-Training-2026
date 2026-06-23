0cf0cb9d93a18699d8408ef679c1aed1b3fc0a7e
shruti verma <shrutiverma261220@gmail.com>
2026-06-23 22:38:45 +0530

    Shruti Verma - Added Exception Handling practice Problem

---
+++ core-java/gcr-codebase/Exception Handling/MultipleCatch.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MultipleCatch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {10,20,30,40,50};
+
+        System.out.print("Enter index: ");
+        int index = sc.nextInt();
+
+        try {
+
+            System.out.println("Value at index " + index + " : " + arr[index]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+            System.out.println("Invalid index!");
+
+        } catch (NullPointerException e) {
+
+            System.out.println("Array is not initialized!");
+        }
+
+    }
+}
---
+++ core-java/gcr-codebase/Exception Handling/NestedTryCatch.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class NestedTryCatch{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {10,20,30,40,50};
+
+        System.out.print("Enter index: ");
+        int index = sc.nextInt();
+
+        System.out.print("Enter divisor: ");
+        int divisor = sc.nextInt();
+
+        try {
+
+            try {
+
+                int value = arr[index];
+
+                try {
+
+                    System.out.println("Result = " + (value / divisor));
+
+                } catch (ArithmeticException e) {
+
+                    System.out.println("Cannot divide by zero!");
+                }
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+
+                System.out.println("Invalid array index!");
+            }
+
+        } catch (Exception e) {
+
+            System.out.println("Unknown Error");
+        }
+
+    }
+}
