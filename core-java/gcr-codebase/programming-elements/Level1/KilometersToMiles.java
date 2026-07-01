975620daddd3a8613c2af15d9c6d1dca2d114fef
shruti verma <shrutiverma261220@gmail.com>
2026-07-01 13:52:56 +0530

    [Shruti Verma] deleted core java folder

--- core-java/gcr-codebase/programming-elements/Level1/KilometersToMiles.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class KilometersToMiles {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double miles = kilometers / 1.6;
-
-        System.out.println("The distance " + kilometers + " km in miles is " + miles);
-    }
-}
